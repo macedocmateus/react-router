d466bc40505484b7a25f0bdeb23cdefe32f6da7a
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-15 18:47:59 -0300

    create 404 page not found

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export function NoteFound() {
+    return (
+        <div>
+            <h1>Ops! essa página não existe!</h1>
+            <a href="/">Voltar para inicio</a>
+        </div>
+    );
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router";
 import { Home } from "../pages/Home";
 import { Products } from "../pages/Products";
+import { NoteFound } from "../pages/NotFound";
 
 export function AppRoutes() {
     return (
         <Routes>
             <Route path="/" index element={<Home />}></Route>
             <Route path="/products" element={<Products />}></Route>
+            <Route path="*" element={<NoteFound />}></Route>
         </Routes>
     );
 }
