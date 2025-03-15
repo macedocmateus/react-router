a4b830b6c9719ef27af4ffe6a307b677ba0a4d9e
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-15 19:16:31 -0300

    add useNavigate for navigate through method

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,25 @@
+import { useNavigate } from "react-router";
+
 export function Home() {
+    const navigate = useNavigate();
+
+    function productsOpen() {
+        navigate("/products");
+    }
+
     return (
         <div>
             <h1>Página Home</h1>
 
             <nav>
-                <a href="/products"> Ver produtos</a>
+                <a href="/products"> Produtos</a>
 
                 {/* Criando uma categoria */}
                 <a href="/products?category=tvs&price=70"> Ver categoria</a>
+
+                <button type="button" onClick={productsOpen}>
+                    Ver produtos
+                </button>
             </nav>
         </div>
     );
