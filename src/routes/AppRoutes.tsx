225591a1c25fa16b1101974fa0530e981bfb70d8
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-15 19:24:14 -0300

    add route with parameter, using id

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import { Home } from "../pages/Home";
 import { Products } from "../pages/Products";
 import { NoteFound } from "../pages/NotFound";
+import { Details } from "../pages/details";
 
 export function AppRoutes() {
     return (
@@ -9,6 +10,7 @@ export function AppRoutes() {
             <Route path="/" index element={<Home />}></Route>
             <Route path="/products" element={<Products />}></Route>
             <Route path="*" element={<NoteFound />}></Route>
+            <Route path="/details/:id" element={<Details />}></Route>
         </Routes>
     );
 }
