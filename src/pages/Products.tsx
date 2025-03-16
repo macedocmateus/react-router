cb691ab6cad3bbe641d548b2a8a5d3fd6ec82f02
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-16 14:31:49 -0300

    add details page for each products using useParams

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -18,6 +18,14 @@ export function Products() {
                     Preço: <strong>{price}</strong>
                 </span>
             )}
+
+            <div className="cards">
+                <a href="/details/1">Produto 1</a>
+                <a href="/details/2">Produto 2</a>
+                <a href="/details/3">Produto 3</a>
+                <a href="/details/4">Produto 4</a>
+                <a href="/details/5">Produto 5</a>
+            </div>
         </div>
     );
 }
